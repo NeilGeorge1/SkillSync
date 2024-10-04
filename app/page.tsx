27b4e083ecc3676ef363d1c121d14dcfaf04e1d0
Aryan <iamaryan.ag@gmail.com>
2024-10-04 11:14:12 +0530

    Removing black box in background

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,15 @@ export default function Home() {
     <div>
       <Navbar />
       {/* Hero Section */}
-      <div className="hero min-h-screen bg-black">
+      <div className="hero min-h-screen">
         <div className="hero-content text-center">
           <div className="max-w-2xl mx-auto">
             <h1 className="text-6xl font-semibold text-white leading-tight">
               Grow your skills everyday
             </h1>
             <p className="py-6 text-xl text-gray-400">
-              Discover the best mentors and mentees. Take the first step to reach the future you want.
+              Discover the best mentors and mentees. Take the first step to
+              reach the future you want.
             </p>
             <a className="btn btn-outline btn-black text-white text-xl px-8 py-3 hover:bg-white hover:text-black">
               Get Started
